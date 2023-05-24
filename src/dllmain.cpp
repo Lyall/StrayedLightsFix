fc2389e8000b07457cd1ee752de61d11af3f46e0
Lyall <lyall@stamper.net>
2023-05-24 21:35:09 +0100

    Fix old aspect code.

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -95,7 +95,7 @@ void __declspec(naked) AspectRatio_CC()
     __asm
     {
         or dword ptr[rbx + 0x30], 01           // Original code
-        mov [rbx + 0x2C], 0x3FE38BAC            // Original code
+        mov dword ptr[rbx + 0x2C], 0x3FE38BAC  // Original code
         add rsp, 32                            // Original code
 
         cmp [iAspectFix], 1
