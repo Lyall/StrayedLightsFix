3063ef71a496ee3d9a2bfb6484f84880ffc280f9
Lyall Stamper <lyall@stamper.net>
2024-04-27 17:03:29 +0100

    Rewrite.

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,277 +1,219 @@
 #include "stdafx.h"
 #include "helper.hpp"
-
-using namespace std;
+#include <inipp/inipp.h>
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <safetyhook.hpp>
 
 HMODULE baseModule = GetModuleHandle(NULL);
+HMODULE thisModule;
 
+// Logger and config setup
 inipp::Ini<char> ini;
+std::shared_ptr<spdlog::logger> logger;
+string sFixName = "StrayedLightsFix";
+string sFixVer = "1.0.0";
+string sLogFile = "StrayedLightsFix.log";
+string sConfigFile = "StrayedLightsFix.ini";
+string sExeName;
+filesystem::path sExePath;
+filesystem::path sThisModulePath;
+std::pair DesktopDimensions = { 0,0 };
 
-// INI Variables
+// Ini Variables
+
 bool bAspectFix;
 bool bFOVFix;
-int iCustomResX;
-int iCustomResY;
-int iInjectionDelay;
-float fAdditionalFOV;
-int iAspectFix;
-int iFOVFix;
+
+// Aspect ratio + HUD stuff
+float fPi = (float)3.141592653;
+float fNativeAspect = (float)16 / 9;
+float fNativeWidth;
+float fNativeHeight;
+float fAspectRatio;
+float fAspectMultiplier;
+float fHUDWidth;
+float fHUDHeight;
+float fDefaultHUDWidth = (float)1920;
+float fDefaultHUDHeight = (float)1080;
+float fHUDWidthOffset;
+float fHUDHeightOffset;
 
 // Variables
-float fNewX;
-float fNewY;
-float fNativeAspect = (float)16/9;
-float fPi = 3.14159265358979323846f;
-float fNewAspect;
-string sFixVer = "1.0.0";
-
-// CurrResolution Hook
-DWORD64 CurrResolutionReturnJMP;
-void __declspec(naked) CurrResolution_CC()
-{
-    __asm
-    {
-        mov r12d, r9d                          // Original code
-        mov rbx, rdx                           // Original code
-        mov rdi, [rax + r8 * 0x8]              // Original code
-        add rdi, rcx                           // Original code
-        mov eax, [rdi]                         // Original code
-
-        mov[iCustomResX], r15d                 // Grab current resX
-        mov[iCustomResY], r12d                 // Grab current resY
-        cvtsi2ss xmm14, r15d
-        cvtsi2ss xmm15, r12d
-        divss xmm14,xmm15
-        movss [fNewAspect], xmm14              // Grab current aspect ratio
-        xorps xmm14,xmm14
-        xorps xmm15,xmm15
-        jmp[CurrResolutionReturnJMP]
-    }
-}
-
-// Aspect Ratio/FOV Hook
-DWORD64 FOVFixReturnJMP;
-float FOVPiDiv;
-float FOVDivPi;
-float FOVFinalValue;
-void __declspec(naked) FOVFix_CC()
-{
-    __asm
-    {
-        mov eax, [fNewAspect]
-        cmp eax, [fNativeAspect]
-        jna originalCode                       // Skip FOVFix if aspect ratio <=1.77778
-        cmp [iFOVFix], 1                       // Check if FOVFix is enabled
-        je modifyFOV                           // jmp to FOV fix
-        jmp originalCode                       // jmp to originalCode
-
-        modifyFOV:
-            fld dword ptr[rbx + 0x1F8]         // Push original FOV to FPU register st(0)
-            fmul[FOVPiDiv]                     // Multiply st(0) by Pi/360
-            fptan                              // Get partial tangent. Store result in st(1). Store 1.0 in st(0)
-            fxch st(1)                         // Swap st(1) to st(0)
-            fdiv[fNativeAspect]                // Divide st(0) by 1.778~
-            fmul[fNewAspect]                   // Multiply st(0) by new aspect ratio
-            fxch st(1)                         // Swap st(1) to st(0)
-            fpatan                             // Get partial arc tangent from st(0), st(1)
-            fmul[FOVDivPi]                     // Multiply st(0) by 360/Pi
-            fadd[fAdditionalFOV]               // Add additional FOV
-            fstp[FOVFinalValue]                // Store st(0) 
-            movss xmm0, [FOVFinalValue]        // Copy final FOV value to xmm0
-            jmp originalCode
-
-        originalCode:
-            movss[rdi + 0x18], xmm0            // Original code
-            mov eax, [rbx + 0x00000208]        // Original code
-            mov[rdi + 0x2C], eax               // Original code
-            jmp [FOVFixReturnJMP]                      
-    }
-}
-
-// Aspect Ratio Hook
-DWORD64 AspectRatioReturnJMP;
-void __declspec(naked) AspectRatio_CC()
-{
-    __asm
-    {
-        or dword ptr[rbx + 0x30], 01           // Original code
-        mov dword ptr[rbx + 0x2C], 0x3FE38BAC  // Original code
-        add rsp, 32                            // Original code
-
-        cmp [iAspectFix], 1
-        jne home
-        push rax
-        mov eax, [fNewAspect]
-        mov [rbx + 0x2C], eax
-        pop rax
-        jmp home
-
-        home:
-            jmp[AspectRatioReturnJMP]
-    }
-}
-
-// FOV Culling Hook
-DWORD64 FOVCullingReturnJMP;
-float fOne = (float)1;
-void __declspec(naked) FOVCulling_CC()
-{
-    __asm
-    {
-        movss xmm1, [fOne]                      // r.StaticMeshLODDistanceScale | 1 = default, higher is worse
-        movss [rdx + 0x000002E8], xmm1          // Original code
-        xor r8d, r8d                            // Original code
-        movsd xmm0, [rbp + 0x10]                // Original code
-        jmp[FOVCullingReturnJMP]
-    }
-}
+int iResX;
+int iResY;
 
 void Logging()
 {
-    loguru::add_file("StrayedLightsFix.log", loguru::Truncate, loguru::Verbosity_MAX);
-    loguru::set_thread_name("Main");
+    // Get this module path
+    WCHAR thisModulePath[_MAX_PATH] = { 0 };
+    GetModuleFileNameW(thisModule, thisModulePath, MAX_PATH);
+    sThisModulePath = thisModulePath;
+    sThisModulePath = sThisModulePath.remove_filename();
 
-    LOG_F(INFO, "StrayedLightsFix v%s loaded", sFixVer.c_str());
+    // Get game name and exe path
+    WCHAR exePath[_MAX_PATH] = { 0 };
+    GetModuleFileNameW(baseModule, exePath, MAX_PATH);
+    sExePath = exePath;
+    sExeName = sExePath.filename().string();
+    sExePath = sExePath.remove_filename();
+
+    // spdlog initialisation
+    {
+        try
+        {
+            logger = spdlog::basic_logger_st(sFixName.c_str(), sThisModulePath.string() + sLogFile, true);
+            spdlog::set_default_logger(logger);
+
+            spdlog::flush_on(spdlog::level::debug);
+            spdlog::info("----------");
+            spdlog::info("{} v{} loaded.", sFixName.c_str(), sFixVer.c_str());
+            spdlog::info("----------");
+            spdlog::info("Path to logfile: {}", sThisModulePath.string() + sLogFile);
+            spdlog::info("----------");
+
+            // Log module details
+            spdlog::info("Module Name: {0:s}", sExeName.c_str());
+            spdlog::info("Module Path: {0:s}", sExePath.string());
+            spdlog::info("Module Address: 0x{0:x}", (uintptr_t)baseModule);
+            spdlog::info("Module Timestamp: {0:d}", Memory::ModuleTimestamp(baseModule));
+            spdlog::info("----------");
+        }
+        catch (const spdlog::spdlog_ex& ex)
+        {
+            AllocConsole();
+            FILE* dummy;
+            freopen_s(&dummy, "CONOUT$", "w", stdout);
+            std::cout << "Log initialisation failed: " << ex.what() << std::endl;
+        }
+    }
 }
 
 void ReadConfig()
 {
-    // Initialize config
-    // UE4 games use launchers so config path is relative to launcher
-
-    std::ifstream iniFile(".\\Aether\\Binaries\\Win64\\StrayedLightsFix.ini");
+    // Initialise config
+    std::ifstream iniFile(sThisModulePath.string() + sConfigFile);
     if (!iniFile)
     {
-        LOG_F(ERROR, "Failed to load config file.");
-        LOG_F(ERROR, "Trying alternate config path.");
-        std::ifstream iniFile("StrayedLightsFix.ini");
-        if (!iniFile)
-        {
-            LOG_F(ERROR, "Failed to load config file. (Alternate path)");
-            LOG_F(ERROR, "Please ensure that the ini configuration file is in the correct place.");
-        }
-        else
-        {
-            ini.parse(iniFile);
-            LOG_F(INFO, "Successfuly loaded config file. (Alternate path)");
-        }
+        AllocConsole();
+        FILE* dummy;
+        freopen_s(&dummy, "CONOUT$", "w", stdout);
+        std::cout << "" << sFixName.c_str() << " v" << sFixVer.c_str() << " loaded." << std::endl;
+        std::cout << "ERROR: Could not locate config file." << std::endl;
+        std::cout << "ERROR: Make sure " << sConfigFile.c_str() << " is located in " << sThisModulePath.string().c_str() << std::endl;
     }
     else
     {
+        spdlog::info("Path to config file: {}", sThisModulePath.string() + sConfigFile);
         ini.parse(iniFile);
-        LOG_F(INFO, "Successfuly loaded config file.");
     }
 
-    inipp::get_value(ini.sections["StrayedLightsFix Parameters"], "InjectionDelay", iInjectionDelay);
+    // Read ini file
     inipp::get_value(ini.sections["Fix Aspect Ratio"], "Enabled", bAspectFix);
-    iAspectFix = (int)bAspectFix;
     inipp::get_value(ini.sections["Fix FOV"], "Enabled", bFOVFix);
-    iFOVFix = (int)bFOVFix;
-    inipp::get_value(ini.sections["Fix FOV"], "AdditionalFOV", fAdditionalFOV);
-
-    // Custom resolution
-    if (iCustomResX > 0 && iCustomResY > 0)
-    {
-        fNewX = (float)iCustomResX;
-        fNewY = (float)iCustomResY;
-        fNewAspect = (float)iCustomResX / (float)iCustomResY;
-    }
-    else
-    {
-        // Grab desktop resolution
-        RECT desktop;
-        GetWindowRect(GetDesktopWindow(), &desktop);
-        fNewX = (float)desktop.right;
-        fNewY = (float)desktop.bottom;
-        iCustomResX = (int)desktop.right;
-        iCustomResY = (int)desktop.bottom;
-        fNewAspect = (float)desktop.right / (float)desktop.bottom;
-    }
 
     // Log config parse
-    LOG_F(INFO, "Config Parse: iInjectionDelay: %dms", iInjectionDelay);
-    LOG_F(INFO, "Config Parse: bAspectFix: %d", bAspectFix);
-    LOG_F(INFO, "Config Parse: bFOVFix: %d", bFOVFix);
-    LOG_F(INFO, "Config Parse: fAdditionalFOV: %.2f", fAdditionalFOV);
-    LOG_F(INFO, "Config Parse: iCustomResX: %d", iCustomResX);
-    LOG_F(INFO, "Config Parse: iCustomResY: %d", iCustomResY);
-    LOG_F(INFO, "Config Parse: fNewX: %.2f", fNewX);
-    LOG_F(INFO, "Config Parse: fNewY: %.2f", fNewY);
-    LOG_F(INFO, "Config Parse: fNewAspect: %.4f", fNewAspect);
+    spdlog::info("Config Parse: bAspectFix: {}", bAspectFix);
+    spdlog::info("Config Parse: bFOVFix: {}", bFOVFix);
+    spdlog::info("----------");
+
+    // Get desktop resolution
+    DesktopDimensions = Util::GetPhysicalDesktopDimensions();
 }
 
-void FOVFix()
+void AspectFOV()
 {
+    uint8_t* CurrResolutionScanResult = Memory::PatternScan(baseModule, "33 ?? B9 ?? ?? ?? ?? 45 ?? ?? 48 ?? ?? 4A ?? ?? ?? 48 ?? ?? 8B ??");
+    if (CurrResolutionScanResult)
+    {
+        spdlog::info("Aspect Ratio: Address is {:s}+{:x}", sExeName.c_str(), (uintptr_t)CurrResolutionScanResult - (uintptr_t)baseModule);
+
+        static SafetyHookMid CurrResolutionMidHook{};
+        CurrResolutionMidHook = safetyhook::create_mid(CurrResolutionScanResult + 0x7,
+            [](SafetyHookContext& ctx)
+            {
+                iResX = ctx.r15;
+                iResY = ctx.r12;
+
+                fAspectRatio = (float)iResX / iResY;
+                fAspectMultiplier = fAspectRatio / fNativeAspect;
+                fNativeWidth = (float)iResY * fNativeAspect;
+                fNativeHeight = (float)iResX / fNativeAspect;
+
+                // HUD variables
+                fHUDWidth = (float)iResY * fNativeAspect;
+                fHUDHeight = (float)iResY;
+                fHUDWidthOffset = (float)(iResX - fHUDWidth) / 2;
+                fHUDHeightOffset = 0;
+                if (fAspectRatio < fNativeAspect)
+                {
+                    fHUDWidth = (float)iResX;
+                    fHUDHeight = (float)iResX / fNativeAspect;
+                    fHUDWidthOffset = 0;
+                    fHUDHeightOffset = (float)(iResY - fHUDHeight) / 2;
+                }
+            });
+    }
+    else if (!CurrResolutionScanResult)
+    {
+        spdlog::error("Current Resolution: Pattern scan failed.");
+    }
+
+
     if (bAspectFix)
     {
-        uint8_t* CurrResolutionScanResult = Memory::PatternScan(baseModule, "33 ?? B9 ?? ?? ?? ?? 45 ?? ?? 48 ?? ?? 4A ?? ?? ?? 48 ?? ?? 8B ??");
-        if (CurrResolutionScanResult)
-        {
-            DWORD64 CurrResolutionAddress = (uintptr_t)CurrResolutionScanResult + 0x7;
-            int CurrResolutionHookLength = Memory::GetHookLength((char*)CurrResolutionAddress, 13);
-            CurrResolutionReturnJMP = CurrResolutionAddress + CurrResolutionHookLength;
-            Memory::DetourFunction64((void*)CurrResolutionAddress, CurrResolution_CC, CurrResolutionHookLength);
-
-            LOG_F(INFO, "Current Resolution: Hook length is %d bytes", CurrResolutionHookLength);
-            LOG_F(INFO, "Current Resolution: Hook address is 0x%" PRIxPTR, (uintptr_t)CurrResolutionAddress);
-        }
-        else if (!CurrResolutionScanResult)
-        {
-            LOG_F(INFO, "Current Resolution: Pattern scan failed.");
-        }
-
-        uint8_t* AspectRatioScanResult = Memory::PatternScan(baseModule, "83 ?? ?? 01 C7 ?? ?? ?? ?? E3 3F 48 ?? ?? ");
+        // Aspect Ratio
+        uint8_t* AspectRatioScanResult = Memory::PatternScan(baseModule, "89 ?? ?? 0F ?? ?? ?? ?? ?? 00 33 ?? ?? 83 ?? 01");
         if (AspectRatioScanResult)
         {
-            DWORD64 AspectRatioAddress = (uintptr_t)AspectRatioScanResult;
-            int AspectRatioHookLength = Memory::GetHookLength((char*)AspectRatioAddress, 13);
-            AspectRatioReturnJMP = AspectRatioAddress + AspectRatioHookLength;
-            Memory::DetourFunction64((void*)AspectRatioAddress, AspectRatio_CC, AspectRatioHookLength);
+            spdlog::info("Aspect Ratio: Address is {:s}+{:x}", sExeName.c_str(), (uintptr_t)AspectRatioScanResult - (uintptr_t)baseModule);
 
-            LOG_F(INFO, "Aspect Ratio: Hook length is %d bytes", AspectRatioHookLength);
-            LOG_F(INFO, "Aspect Ratio: Hook address is 0x%" PRIxPTR, (uintptr_t)AspectRatioAddress);
+            static SafetyHookMid AspectRatioMidHook{};
+            AspectRatioMidHook = safetyhook::create_mid(AspectRatioScanResult ,
+                [](SafetyHookContext& ctx)
+                {
+                    ctx.rax = *(uint32_t*)&fAspectRatio;
+                });
         }
         else if (!AspectRatioScanResult)
         {
-            LOG_F(INFO, "Aspect Ratio: Pattern scan failed.");
+            spdlog::error("Aspect Ratio: Pattern scan failed.");
         }
     }
 
     if (bFOVFix)
     {
-        uint8_t* FOVFixScanResult = Memory::PatternScan(baseModule, "F3 0F ?? ?? ?? ?? ?? ?? F3 0F ?? ?? ?? 8B ?? ?? ?? ?? ?? 89 ?? ?? 0F ?? ?? ?? ?? ?? ?? 33 ?? ?? 83 ?? ??");
-        if (FOVFixScanResult)
-        {
-            FOVPiDiv = fPi / 360;
-            FOVDivPi = 360 / fPi;
-
-            DWORD64 FOVFixAddress = (uintptr_t)FOVFixScanResult + 0x8;
-            int FOVFixHookLength = Memory::GetHookLength((char*)FOVFixAddress, 13);
-            FOVFixReturnJMP = FOVFixAddress + FOVFixHookLength;
-            Memory::DetourFunction64((void*)FOVFixAddress, FOVFix_CC, FOVFixHookLength);
-
-            LOG_F(INFO, "FOV: Hook length is %d bytes", FOVFixHookLength);
-            LOG_F(INFO, "FOV: Hook address is 0x%" PRIxPTR, (uintptr_t)FOVFixAddress);
-        }
-        else if (!FOVFixScanResult)
-        {
-            LOG_F(INFO, "FOV: Pattern scan failed.");
-        }
-
+        // FOV
+        uint8_t* FOVScanResult = Memory::PatternScan(baseModule, "F3 0F ?? ?? ?? ?? ?? ?? F3 0F ?? ?? ?? 8B ?? ?? ?? ?? ?? 89 ?? ?? 0F ?? ?? ?? ?? ?? ?? 33 ?? ?? 83 ?? ??");
         uint8_t* FOVCullingScanResult = Memory::PatternScan(baseModule, "83 ?? ?? ?? ?? ?? 00 49 ?? ?? ?? 74 ?? 48 ?? ?? ?? ?? ?? 00");
-        if (FOVCullingScanResult)
+        if (FOVScanResult && FOVCullingScanResult)
         {
-                DWORD64 FOVCullingAddress = (uintptr_t)FOVCullingScanResult + 0x46;
-                int FOVCullingHookLength = Memory::GetHookLength((char*)FOVCullingAddress, 13);
-                FOVCullingReturnJMP = FOVCullingAddress + FOVCullingHookLength;
-                Memory::DetourFunction64((void*)FOVCullingAddress, FOVCulling_CC, FOVCullingHookLength);
+            spdlog::info("FOV: Address is {:s}+{:x}", sExeName.c_str(), (uintptr_t)FOVScanResult - (uintptr_t)baseModule);
+            spdlog::info("FOV Culling: Address is {:s}+{:x}", sExeName.c_str(), (uintptr_t)FOVCullingScanResult - (uintptr_t)baseModule);
 
-                LOG_F(INFO, "FOV Culling: Hook length is %d bytes", FOVCullingHookLength);
-                LOG_F(INFO, "FOV Culling: Hook address is 0x%" PRIxPTR, (uintptr_t)FOVCullingAddress);
+            static SafetyHookMid FOVMidHook{};
+            FOVMidHook = safetyhook::create_mid(FOVScanResult + 0x8,
+                [](SafetyHookContext& ctx)
+                {
+                    if (fAspectRatio > fNativeAspect)
+                    {
+                        ctx.xmm0.f32[0] = atanf(tanf(ctx.xmm0.f32[0] * (fPi / 360)) / fNativeAspect * fAspectRatio) * (360 / fPi);
+                    }
+                });
+
+            static SafetyHookMid FOVCullingMidHook{};
+            FOVCullingMidHook = safetyhook::create_mid(FOVCullingScanResult + 0x46,
+                [](SafetyHookContext& ctx)
+                {
+                    if (fAspectRatio > fNativeAspect)
+                    {
+                        ctx.xmm1.f32[0] = 1.0f;
+                    }
+                });
         }
-        else if (!FOVCullingScanResult)
+        else if (!FOVScanResult || !FOVCullingScanResult)
         {
-            LOG_F(INFO, "FOV Culling: Pattern scan failed.");
+            spdlog::error("FOV: Pattern scan failed.");
         }
     }
 }
@@ -280,9 +222,8 @@ DWORD __stdcall Main(void*)
 {
     Logging();
     ReadConfig();
-    Sleep(iInjectionDelay);
-    FOVFix();
-    return true; // end thread
+    AspectFOV();
+    return true;
 }
 
 BOOL APIENTRY DllMain( HMODULE hModule,
@@ -294,10 +235,11 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     {
     case DLL_PROCESS_ATTACH:
     {
+        thisModule = hModule;
         HANDLE mainHandle = CreateThread(NULL, 0, Main, 0, NULL, 0);
-
         if (mainHandle)
         {
+            SetThreadPriority(mainHandle, THREAD_PRIORITY_HIGHEST); // set our Main thread priority higher than the games thread
             CloseHandle(mainHandle);
         }
     }
--- src/stdafx.h
+++ src/stdafx.h
@@ -3,12 +3,7 @@
 #define WIN32_LEAN_AND_MEAN
 
 #include <cassert>
-#include <Shlobj.h>
-#include <algorithm>
+#include <Windows.h>
 #include <fstream>
-#include <string>
 #include <inttypes.h>
-
-#include "external/loguru/loguru.hpp"
-#include "external/inipp/inipp/inipp.h"
-#include "external/length-disassembler/headerOnly/ldisasm.h"
+#include <filesystem>
